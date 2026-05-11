7fac816b5cace38d7af244c67c3c31de4a9bf939
Hillary Kibet <155447735+hillary445@users.noreply.github.com>
2026-05-11 15:59:09 +0300

    Update auth.middleware.js

--- src/midlleware/auth.middleware.js
+++ src/midlleware/auth.middleware.js
@@ -1,33 +1,33 @@
 require('dotenv').config();
-import jwt from "jsonwebtoken";
-import { verifyToken } from "../utils/jwt.utils";
+const { verifyToken } = require("../utils/jwt.utils");
+const ErrorResponse = require("../utils/ErrorObj");
+
 
 //Verifes the JWT in the authorization header
 //on success , attaches decoded payload to req.user on failure, returns 401
-export const authenticate = (req, res, next) => {
+const authenticate = (req, res, next) => {
     const authHeader = req.headers.authorization
     if (!authHeader?.startsWith('Bearer')) {
-        return res.status(401).json({success:false, message: 'No token provided' })
+        return next(new ErrorResponse('No token provided', 401)
     }
     const token = authHeader.split(' ')[1]
     try {
         const decoded = verifyToken(token)
         req.user = decoded
         next()
-    } catch {
-        return res.status(401).json({success:false, message: 'Invalid or expired token.Please log in again'})
+    } catch (error) {
+        return next(new ErrorResponse('Invalid or expired token. Please log in again', 401));
     }
 }
 
 //Restricts a route to a specific role
-export const authorise = (...roles) => {
+const authorise = (...Role) => {
     return (req, res, next) => {
-        if (!roles.includes(req.user.role)){
-            return res.status(403).json({
-                success:false,
-                message:"You do not have permission to access this resource"
-            });
+        if (!Role.includes(req.user.role)){
+            return next(new ErrorResponse('You do not have permission to access this resource', 403));
         }
         next();
     };
 };
+
+module.exports = { authenticate, authorise}
