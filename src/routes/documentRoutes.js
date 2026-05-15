0bcc797b8b8ab35c3afef746595357208ab6fae2
devbysamcloudy <snganga685@gmail.com>
2026-05-15 20:24:19 +0300

    fix: destructure upload from uploader utils

--- src/routes/documentRoutes.js
+++ src/routes/documentRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const upload = require("../utils/uploader");
+// documentRoutes.js
+const { upload } = require("../utils/uploader");
 const {
   uploadDocument,
   getDocumentById,
