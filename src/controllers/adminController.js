82cd1c5fd1c3c4adfe8a4c7a53dbd675b6fd4d6e
HALIMA-254-aft <halmhassan26@gmail.com>
2026-05-15 10:08:34 +0300

    removed the hard coded codes

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -25,24 +25,18 @@ module.exports.approveLawyer = async (req, res, next) => {
 
       // 2. CREATE LAWYER PROFILE (This will show APPROVAL)
       const newProfile = await createLawyer(tx, {
-        lawyer_application_id: application.id,
-        category: "General Practice",
-        phone_number: "0712345678",
-        description: "Verified Advocate of the High Court",
-        experience: 5,
-        consultation_fee: 3000,
-        is_active: true,
-      });
-
-      // 3. UPDATE USER ROLE
-      const updatedUser = await tx.users.update({
-        where: { id: application.user_id },
-        data: { role: "LAWYER" },
-      });
+   lawyer_application_id: application.id,
+   category: application.category,
+   phone_number: application.phone_number,
+   description: application.description,
+   experience: application.experience,
+   consultation_fee: application.consultation_fee,
+   is_active: true,
+});
+      
 
       return {
         newProfile,
-        updatedUser,
       };
     });
     
