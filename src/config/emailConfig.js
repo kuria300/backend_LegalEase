d1bbc062a6cdac13b376355dbfd7cfbe7cd81b80
devbysamcloudy <snganga685@gmail.com>
2026-05-16 02:21:01 +0300

    fix: resolve multipart form-data body parsing issues and remove duplicate clearHistory

--- src/config/emailConfig.js
+++ src/config/emailConfig.js
@@ -1,12 +1,10 @@
 require('dotenv').config()
 
-
-module.exports={
-    gmail:{
-      host: process.env.SMTP_HOST,
-      port: process.env.SMTP_PORT,
-      user:process.env.SMTP_USER,
-      password:process.env.SMTP_PASS
-
+module.exports = {
+    gmail: {
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      user: process.env.EMAIL_USER,
+      password: process.env.EMAIL_PASS
     }
 }
